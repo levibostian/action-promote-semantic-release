ca3d42abb3265f4e2a5d32aab92538cc6f5199b5
Levi Bostian <levi@levi.earth>
2022-01-28 11:34:32 -0600

    make rest of script

--- action.js
+++ action.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const process = require('process')
+const {execSync} = require('child_process')
 
 const log = message => {
 	console.log(`[action-promote-semantic-release] ${message}`)
@@ -23,9 +24,17 @@ const logThenExit = (exitCode, message) => {
 	process.exit(exitCode)
 }
 
+const exec = command => {
+	log(`$> ${command}`)
+
+	// Stderr is sent to stderr of parent process
+	const stdout = execSync(command)
+	log(stdout)
+}
+
 startLogGroup('Setting up script...')
 
-const currentBranch = (process.env.GITHUB_REF).replace("refs/heads/", "") // Branch that the workflow got triggered from. In format: "refs/heads/<branch-name>"
+const currentBranch = (process.env.GITHUB_REF).replace('refs/heads/', '') // Branch that the workflow got triggered from. In format: "refs/heads/<branch-name>"
 log(`Branch workflow triggered from: ${currentBranch}`)
 let sequence = process.argv.slice(2)[0] // First 2 args are command and file name of script. Remove those.
 sequence = sequence.split(',')
@@ -46,7 +55,30 @@ if ((sequence.indexOf(currentBranch) + 1) < sequence.length) {
 	branchAhead = sequence[sequence.indexOf(currentBranch) + 1]
 }
 
+if (!branchAhead) {
+	logThenExit(0, 'No branch to promote the current branch to. Nothing for me to do here. Exiting.')
+}
+
 log(`Branch ahead current in sequence: ${branchAhead || 'none'}`)
 
 endLogGroup()
 
+startLogGroup(`Switch to branch ${branchAhead} and copy over commits.`)
+
+log('Checkout branch or create if not exist.')
+exec(`git switch --create ${branchAhead}`)
+log(`Merge in commits. If branch ${branchAhead} just got created, this command will not do anything.`)
+exec(`git merge --ff ${currentBranch}`)
+
+startLogGroup(`Pushing changes to branch ${branchAhead}`)
+exec('git push')
+endLogGroup()
+
+startLogGroup('Cleanup...')
+if (branchBehind) {
+	log('There is a branch behind this one in the sequence. That means this branch is a temporary release branch. Action will delete current branch.')
+	log(`Deleting branch: ${currentBranch}`)
+	exec(`git push origin --delete ${currentBranch}`)
+}
+
+endLogGroup()
